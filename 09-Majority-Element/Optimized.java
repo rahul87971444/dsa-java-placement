eae209968e1e04dd2680161ef4c78fab31467477
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-27 19:46:05 +0530

    Implement Boyer-Moore Voting Algorithm in Optimized.java

---
+++ 09-Majority-Element/Optimized.java
@@ -0,0 +1,20 @@
+public class Optimized {
+    public static void main(String[] args) {
+        int nums[]={2,2,1,1,1,2,2};
+        int candidate=0;
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                candidate=num;
+            }
+            if(candidate==num){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        System.out.println(candidate);
+
+    }
+
+}
