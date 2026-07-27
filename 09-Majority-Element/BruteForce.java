aada19f4d50c6c33ce496211d4b64b3db9792240
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-27 19:42:15 +0530

    Implement brute force solution for majority element

---
+++ 09-Majority-Element/BruteForce.java
@@ -0,0 +1,20 @@
+public class BruteForce {
+    public static void main(String[] args) {
+        int nums[] = {2,2,1,1,1,2,2};
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j])
+                    count++;
+            }
+            if(count>nums.length/2){
+                System.out.println(nums[i]);
+                return;
+
+            }
+
+        }
+
+    }
+
+}
