b9bd0d5ebb7f46f2272155208820b3a7f9780bf9
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-31 22:06:46 +0530

    Implement optimized solution for trapping rainwater

---
+++ 12-Trapping-Rain-Water/Optimized.java
@@ -0,0 +1,26 @@
+public class Optimized {
+    public static void main(String[] args) {
+        int[] height = {4,2,0,3,2,5};
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    water += leftMax - height[left];
+                left++;
+            }else{
+                if(height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    water += rightMax - height[right];
+                right--;
+            }
+        }
+        System.out.println(water);
+    }
+}
