eda1bb8f99c7170b275f37c608e407c99d0005be
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-08-03 17:59:28 +0530

    Implement optimized solution for longest consecutive sequence

---
+++ 15-Longest-Consecutive-Sequence/Optimized.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+public class Optimized {
+    public static void main(String[] args) {
+        int[] nums = {100,4,200,1,3,2};
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int length = 1;
+                while (set.contains(current + 1)) {
+                    current++;
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        System.out.println("Longest = " + longest);
+    }
+}
