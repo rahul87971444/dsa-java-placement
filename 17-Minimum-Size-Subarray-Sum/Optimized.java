c3c662d6aee1a7076ed948882772de1c2f1bcc25
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-08-08 08:21:02 +0530

    Implement optimized solution for minimum size subarray sum

---
+++ 17-Minimum-Size-Subarray-Sum/Optimized.java
@@ -0,0 +1,27 @@
+public class Optimized {
+    public static void main(String[] args) {
+        int target = 7;
+        int[] nums = {2,3,1,2,4,3};
+        int left = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+        for (int right = 0;
+             right < nums.length;
+             right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                minLength = Math.min(
+                    minLength,
+                    right - left + 1
+                );
+                sum -= nums[left];
+                left++;
+            }
+        }
+        if (minLength == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(minLength);
+        }
+    }
+}
